167bcc0da89c68473c25f0f4e38019122bfc716e
Joel Henriksson <carljoelhenriksson@gmail.com>
2026-01-26 10:21:20 +0100

    Add estimation module with stubs

---
+++ ess_beam_imaging/estimate.py
@@ -0,0 +1,12 @@
+def estimate_with_optimizer(image, meta=None, config=None):
+    """
+    Estimate beam parameters from an image using optimization.
+    """
+    raise NotImplementedError
+
+
+def estimate_with_mcmc(image, meta=None, config=None):
+    """
+    Estimate beam parameters from an image using MCMC.
+    """
+    raise NotImplementedError
